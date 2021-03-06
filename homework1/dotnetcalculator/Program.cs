779d09073ddf01cd543c516f9f2047a9c6ec9f0c
geniusgao-ai <73426298+geniusgao-ai@users.noreply.github.com>
2021-03-06 22:19:56 +0800

    Add files via upload

---
+++ homework1/dotnetcalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace dotnetcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数字!");
+            double num1=Double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字!");
+            double num2=Double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入运算符!");
+            string cal=Console.ReadLine();
+            double result=0;
+            switch (cal)
+            {
+                case "+":result=num1+num2;break;
+                case "-":result=num1-num2;break;
+                case "*":result=num1*num2;break;
+                case "/":result=num1/num2;break;
+                default:break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
