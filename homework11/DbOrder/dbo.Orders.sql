e456ffe0e567750f6f7a8c491e407f60ca379fc4
geniusgao-ai <73426298+geniusgao-ai@users.noreply.github.com>
2021-05-24 12:01:31 +0800

    Add files via upload

---
+++ homework11/DbOrder/dbo.Orders.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Customer]   NVARCHAR (MAX) NULL,
+    [OrderDate]  DATETIME       NULL,
+    [Totalmoney] INT            NULL,
+    CONSTRAINT [PK_dbo.Orders] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
