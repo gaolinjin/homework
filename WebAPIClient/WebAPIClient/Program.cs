7b62be498cf162303b78ed370add86d736b91ba7
geniusgao-ai <73426298+geniusgao-ai@users.noreply.github.com>
2021-05-28 14:18:16 +0800

    Add files via upload

---
+++ WebAPIClient/WebAPIClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient hc = new HttpClient();
+            string html=hc.GetStringAsync("https://localhost:44334/api/Order?i=1?c=ds").Result;
+            Console.WriteLine(html);
+            Console.ReadKey();
+
+
+        }
+    }
+}
